ee7c4f74eeab4ee1428a5d29a4935fafd723f90b
Long Hoang Bao <longhoang0811@gmail.com>
2018-10-07 12:39:10 +0700

    fix bug

--- Dictionary/Word.java
+++ Dictionary/Word.java
@@ -6,29 +6,37 @@ public class Word {
 
     public Word(String wt, String we) {
         word_explain = new ArrayList<String>();
-        word_target = wt;
+        word_target = new String(wt);
         word_explain.add(we);
     }
 
     // setters
     public void setWordTarget(String wt) {
-        word_target = wt;
+        word_target = new String(wt);
     }
 
     public void setWordExplain(ArrayList<String> we) {
-        word_explain.addAll(we);
+        word_explain = new ArrayList<String>();
+        word_explain.addAll(we);    
     }
 
     public void addExplain(String s) {
+        if (word_explain == null) word_explain = new ArrayList<String>();
         word_explain.add(s);
     }
 
     // getters
     public String getWordTarget() {
-        return word_target;
+        return new String(word_target);
     }
 
     public ArrayList<String> getWordExplain() {
-        return word_explain;
+        if (word_explain == null) return null;
+        ArrayList <String> result = new ArrayList<String>();
+        for(String s : word_explain)
+        {
+            result.add(s);
+        }
+        return result;
     }
 }
